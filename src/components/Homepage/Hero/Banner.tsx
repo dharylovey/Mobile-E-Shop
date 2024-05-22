8f04fa2a90ad1bf4e7fd39e62c32d4ea3272a7f1
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-05-22 23:01:56 +0800

    fix image

--- src/components/Homepage/Hero/Banner.tsx
+++ src/components/Homepage/Hero/Banner.tsx
@@ -61,7 +61,7 @@ export default function Banner() {
       </div>
       <div className="flex flex-col justify-center items-center gap-6 self-stretch px-4 py-10 bg-[#EDEDED]">
         <Image
-          src={"/images/Macbook.png"}
+          src={"/images/MacBook.png"}
           alt="banner"
           width={192}
           height={200}
